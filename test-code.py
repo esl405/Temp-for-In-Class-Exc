85de2d1df39ee898af5e7377e8456d4475b881af
esl405 <50837406+esl405@users.noreply.github.com>
2019-06-09 18:34:55 -0400

    WOrking

--- test-code.py
+++ test-code.py
@@ -1,32 +1,22 @@
-#print ("hello world")
-
-#Define F to Celsius Fxn
-
-#c = 1
-#def temp_f(f):
-#    return (f-32) * 5/9
-#c = temp_f (1)    
-#print(c)
-##def convert_temperature(f):
-##    # (32°F − 32) × 5/9 = 0°C
-##    return (f - 32) * 5/9
-##
-##c = convert_temperature(32)
-##print(c) #> 0
-#
-#type("hello")
 import tkinter
+import random
 
-
-from tkinter import *
+#from tkinter import *
 #top = tkinter.Tk()
-root = Tk()
-Widget=Label(None, text="Choose One")
-Widget.pack()
-Button (root, text='Rock', command=root.quit).pack(side=LEFT)
-Button (root, text='Paper', command=root.quit).pack(side=LEFT)
-Button (root, text='Scissor', command=root.quit).pack(side=LEFT)
-root.mainloop()
+
+User_Choice=input(
+    from tkinter import *
+    root = Tk()
+    Widget=Label(None, text="Choose One")
+    Widget.pack()
+    Button (root, text='Rock', command=root.quit).pack(side=LEFT)
+    Button (root, text='Paper', command=root.quit).pack(side=LEFT)
+    Button (root, text='Scissor',  command=root.quit).pack(side=LEFT)
+    Button (root, text='Quit', command=root.quit, fg='red').pack(side=LEFT)
+    root.mainloop()
+)
+
+print(User_Choice)
 #from tkinter import *
 #widget=Label(None,text='testing')
 #widget.pack()
